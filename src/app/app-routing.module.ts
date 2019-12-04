bc4537322f58a629d508b2bf640ec841209a5878
Waylon Dixon <wdixon@waylondixon.com>
2019-12-04 13:01:00 -0700

    Reactive Forms: installed reactiveForm component and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', redirectTo: '/reactive-form', pathMatch: 'full' },
   { path: '', component: HomePageComponent },
+  { path: 'reactive-form', component: ReactiveFormComponent },
   { path: '**', component: PageNotFoundComponent }  // Note: 'wildcard' route should be the last in the routing list
 ];
 
@@ -16,5 +18,6 @@ const routes: Routes = [
 export class AppRoutingModule { }
 export const routingComponents = [
   HomePageComponent,
-  PageNotFoundComponent
+  PageNotFoundComponent,
+  ReactiveFormComponent
 ];
